28e9493a4915b4de5c90d8a75c7cc11be0e52542
Neeraj chauhan <nc1676639@gmail.com>
2026-03-09 19:48:40 +0530

    set up

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express();
+require('dotenv').config()
+const cors=require('cors');
+const connectDb = require('./src/dbconnection/dbConnection');
+
+
+app.use(express.json());
+app.use(cors());
+
+// connectDb();
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+})
---
+++ backend/src/dbconnection/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose= require('mongoose');
+const dotenv=require('dotenv')
+dotenv.config()
+
+const connectDb=async()=>{
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("DATABASE IS CONNECTED")
+        
+    } catch (error) {
+        console.log("ERROR IN DB",error)
+    }
+}
+
+module.exports=connectDb;
