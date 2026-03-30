a64a7c139a9032371da14299dacc3969d3a2630e
Neeraj chauhan <nc1676639@gmail.com>
2026-03-30 10:19:50 +0530

    froontend data

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
-import Protected from "./features/auth/components/Protected";
+import Protected from "./features/auth/components/Protected"
 import Home from "./features/Interview/pages/Home";
-import Interview from "./features/Interview/pages/Interview";
+import Interview from "./features/Interview/pages/Interview"
 
 export const router = createBrowserRouter([
   {
